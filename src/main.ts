f51fa645d746072805f9e22e5e8e286f997b114e
Andrei Shilov <andrei_shilov@epam.com>
2016-09-08 14:04:50 +0300

    Lesson [1] Hello world

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,30 @@
  * Created by Andrei_Shilov on 9/8/2016.
  */
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { AppModule } from './app/app.module';
-if (process.env.ENV === 'production') {
-    enableProdMode();
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserModule} from "@angular/platform-browser";
+import {Component, NgModule} from "@angular/core";
+
+
+@Component({
+    selector: 'app',
+    template: '<div>Hello from app</div>',
+})
+export class AppComponent {
 }
+
+
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
+
+
 platformBrowserDynamic().bootstrapModule(AppModule);
